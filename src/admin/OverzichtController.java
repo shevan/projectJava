beff80de4a8e41ce7850473a2a8840d3f142dd74
Pieter Pletinckx <pieplay@gmail.com>
2014-05-01 15:05:01 +0200

    ListView aan de praat gekregen "Sorry voor de vele commits maar het is een motivatie voor mij"

--- src/admin/OverzichtController.java
+++ src/admin/OverzichtController.java
@@ -3,13 +3,18 @@ package admin;
 import admin.model.Aspnetusers;
 import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Observable;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javax.persistence.EntityManager;
@@ -30,7 +35,12 @@ public class OverzichtController extends AnchorPane implements Initializable
     @FXML
     Label lblOverzicht;
     
+    @FXML
+    ListView <String>tabListView;
+    
     private Main application;
+    
+    final ObservableList<String> listItems = FXCollections.observableArrayList("Add Items Here");
     
     public void setApp(Main application)
     {
@@ -44,12 +54,21 @@ public class OverzichtController extends AnchorPane implements Initializable
 
         TypedQuery<Aspnetusers> queryAspnetusers = em.createNamedQuery("Aspnetusers.findAll", Aspnetusers.class);
         aspnetusers = queryAspnetusers.getResultList();
+        for(Aspnetusers user : aspnetusers)
+        {
+            listItems.add(user.getUserName()+" "+user.getDiscriminator());
+            //set username max char = tab space
+        }
+                
+               
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb)
     { 
         lblOverzicht.setText(aspnetusers.toString());
+        tabListView.setItems(listItems);
+        
     }
     
     public void processStages(ActionEvent event)
